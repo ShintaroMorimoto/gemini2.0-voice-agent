2443d1d53dbeb6050461e10b724bd869f11858da
ShintaroMorimoto <mrmtsntr1@gmail.com>
2024-12-31 14:27:44 +0900

    Audio Worklet関係の処理を追加

--- voice-agent/src/lib/audio-recorder.ts
+++ voice-agent/src/lib/audio-recorder.ts
@@ -1,3 +1,61 @@
+import EventEmitter from 'eventemitter3';
+import { createWorkletFromSrc } from './audio-worklet-registry';
+import AudioRecordingWorklet from './worklets/audio-processing';
+
 function arrayBufferToBase64(buffer: ArrayBuffer) {
-	var binary = '';
+	let binary = '';
+	const bytes = new Uint8Array(buffer);
+	const len = bytes.byteLength;
+	for (let i = 0; i < len; i++) {
+		binary += String.fromCharCode(bytes[i]);
+	}
+	return window.btoa(binary);
+}
+
+export class AudioRecorder extends EventEmitter {
+	stream: MediaStream | undefined;
+	audioContext: AudioContext | undefined;
+	source: MediaStreamAudioSourceNode | undefined;
+	recording = false;
+	recordingWorklet: AudioWorkletNode | undefined;
+
+	private starting: Promise<void> | null = null;
+
+	constructor(public sampleRate = 16000) {
+		super();
+	}
+	async start() {
+		if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+			throw new Error('Cloud not request user media');
+		}
+
+		this.starting = new Promise(async (resolve, reject) => {
+			this.stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+			this.audioContext = new AudioContext({ sampleRate: this.sampleRate });
+			this.source = this.audioContext.createMediaStreamSource(this.stream);
+
+			const workletName = 'audio-recorder-worklet';
+			const src = createWorkletFromSrc(workletName, AudioRecordingWorklet);
+
+			await this.audioContext.audioWorklet.addModule(src);
+			this.recordingWorklet = new AudioWorkletNode(
+				this.audioContext,
+				workletName,
+			);
+
+			this.recordingWorklet.port.onmessage = async (ev: MessageEvent) => {
+				const arrayBuffer = ev.data.data.int16arrayBuffer;
+
+				if (arrayBuffer) {
+					const arrayBufferString = arrayBufferToBase64(arrayBuffer);
+					this.emit('data', arrayBufferString);
+				}
+			};
+			this.source.connect(this.recordingWorklet);
+
+			this.recording = true;
+			resolve();
+			this.starting = null;
+		});
+	}
 }
---
+++ voice-agent/src/lib/audio-worklet-registry.ts
@@ -0,0 +1,21 @@
+export type WorkletGraph = {
+	node?: AudioWorkletNode;
+	handlers: Array<(this: MessagePort, ev: MessageEvent) => any>;
+};
+
+export const registerWorklet: Map<
+	AudioContext,
+	Record<string, WorkletGraph>
+> = new Map();
+
+export const createWorkletFromSrc = (
+	workletName: string,
+	workletSrc: string,
+) => {
+	const script = new Blob(
+		[`registerProcessor('${workletName}', ${workletSrc})`],
+		{ type: 'application/javascript' },
+	);
+
+	return URL.createObjectURL(script);
+};
